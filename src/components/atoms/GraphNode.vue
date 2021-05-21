ffcfcd14496dbbbe7c52a52c56f5a86db400cac7
Anthony <dev.autumnsky@gmail.com>
2021-05-21 22:01:41 +0900

    Add GraphNode Component

---
+++ src/components/atoms/GraphNode.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="graph-node">
+
+  </div>
+  <!-- <p>
+    {{ commit.message }}
+    {{ commit.hash }}
+  </p> -->
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+import { commit } from '@/git/gitTypes'
+
+export default defineComponent({
+  components: {
+  },
+  props: {
+    commit: {
+      type: Object as PropType<commit & {hash: string}>
+    }
+  },
+  setup(props) {
+    // props.commit?.hash
+    return {
+      
+    }
+  },
+})
+</script>
+
+<style scoped>
+.graph-node {
+  width: 50px;
+  height: 50px;
+  border: 4px solid black;
+  border-radius: 50%;
+}
+</style>
