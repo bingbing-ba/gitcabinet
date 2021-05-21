e0ccd0f3dfe568098dc62823ea05cfcef5bbdef2
Anthony <dev.autumnsky@gmail.com>
2021-05-21 22:02:01 +0900

    Add GraphTree Component

---
+++ src/components/molecules/GraphTree.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="graph-tree">
+    <div v-for="(commit, idx) in commits" :key="commit.hash" class="graph-node">
+      <GraphNode :commit="commit" />
+      <div v-if="idx !== commits.length - 1" class="graph-edge"></div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+import { GraphNode } from '@/components'
+import { commit } from '@/git/gitTypes'
+
+export default defineComponent({
+  components: {
+    GraphNode
+  },
+  props: {
+    commits: {
+      type: Object as PropType<commit & {hash: string}>
+    }
+  },
+  setup() {
+    return {
+    }
+  },
+})
+</script>
+
+<style scoped>
+.graph-tree {
+  display: flex;
+}
+
+.graph-node {
+  display: flex;
+  align-items: center;
+}
+
+.graph-edge {
+  height: 1px;
+  width: 50px;
+  border: 2px solid black;
+}
+
+</style>
