8f4dfec6fa5a02052f6681dab1736aa0ffc939a3
Anthony <dev.autumnsky@gmail.com>
2021-05-25 20:17:23 +0900

    Delete hint attribute

--- src/problems/problem2.ts
+++ src/problems/problem2.ts
@@ -7,8 +7,6 @@ problem2.content = `이 디렉토리는 git으로 관리되고 있습니다.
 디렉토리의 README.txt를 staging area에 올리려합니다.
 git status를 통해 현재 상태를 확인해보세요.
 git add README.txt를 통해 README.txt를 staging 해보세요.`
-problem2.hint = `git status
-git add README.txt`
 new PlainFile('README.txt', problem2.refDirectory)
 problem2.setGit(new Git(problem2.refDirectory))
 problem2.setBase()
