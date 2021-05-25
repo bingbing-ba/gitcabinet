b2018d74af06d1a0ccaac30bd3232cb66ca5aae9
Anthony <dev.autumnsky@gmail.com>
2021-05-25 20:42:43 +0900

    Modify function getting problem from props

--- src/components/organisms/GitGraph.vue
+++ src/components/organisms/GitGraph.vue
@@ -6,7 +6,7 @@
       </Title>
     </div>
     <Card class="bg-white pb-10 overflow-y-scroll overflow-x-hidden max-h-full">
-      <NetworkVertical :git="git" :commits="commits" />
+      <NetworkVertical v-if="problem" :git="problem.git" :commits="commits" />
     </Card>
   </div>
 </template>
@@ -15,8 +15,8 @@
 import { computed, defineComponent } from 'vue'
 import { Title, Card, NetworkVertical } from '@/components'
 import { Problem } from "@/problems"
-import { PlainFile, Directory } from '@/git/fileStructure'
-import { Git } from '@/git/git'
+// import { PlainFile, Directory } from '@/git/fileStructure'
+// import { Git } from '@/git/git'
 
 
 export default defineComponent({
@@ -25,109 +25,47 @@ export default defineComponent({
     Card,
     NetworkVertical
   },
-  props:{
-    problem:{
+  props: {
+    problem: {
       type: Problem,
       required: true,
     },
   },
   setup(props) {
-    const presentDir = new Directory('test1')
-    const git = new Git(presentDir)
-    console.log(git)
-    const makeSwitchCommit = (prev: string ,next: string) => {
-      git.head = next
-      git.branches[next] = git.branches[prev]
-
-      const switchATxt = new PlainFile('switch_a.txt', presentDir)
-      git.add()
-      git.commit('Add switch text file A')
-      
-      const switchBTxt = new PlainFile('switch_b.txt', presentDir)
-      git.add()
-      git.commit('Add switch text file B')
-    }
-    
-    const makeSampleCommit = () => {
-      const aTxt = new PlainFile('a.txt', presentDir)
-      git.add()
-      git.commit('Add text file A')
-      const bTxt = new PlainFile('b.txt', presentDir)
-      git.add()
-      git.commit('Add text file B')
-
-      const cTxt = new PlainFile('c.txt', presentDir)
-      git.add()
-      git.commit('Add text file C')
-
-      const dTxt = new PlainFile('d.txt', presentDir)
-      git.add()
-      git.commit('Add text file D')
-
-      // console.log(git)
-      return git
-    }
-
-    makeSampleCommit()
-    makeSwitchCommit('master', 'develop')
-
-    // const commits = computed(() => {
-    //   // const { problem } = props
-    //   // const { git } = problem
-      
-
-    //   // 아직 git init을 하지 않은 경우
-    //   if (git == null) {
-    //     return []
-    //   }
-
-    //   // 브랜치 별 결과 출력 준비
-    //   const result: any = []
-    //   targetBranchs.forEach(head => {
-    //     // 반환할 결과값
-        
-    //     // head : 브랜치 이름
-    //     // const head = git.head
-
-    //     // headCommitHash : 해당 브랜치의 가장 최근 커밋 해쉬
-    //     const headCommitHash = git.branches[head]
-
-    //     // 커밋 기록이 없는 경우
-    //     if (headCommitHash === '') {
-    //       return []
-    //     }
-
-    //     // 기준이 될 commit 노드 설정
-    //     const headCommit = git.commits[headCommitHash]
-
-    //     // 검사를 진행할 스택
-    //     const stack = [
-    //       // 해당 커밋 노드의 hash를 포함한 새로운 객체 
-    //       {
-    //         ...headCommit, 
-    //         hash: headCommitHash
-    //       }
-    //     ]
-        
-    //     // 스택에서 
-    //     while (stack.length !== 0) {
-    //       const commit = stack.pop()
-    //       result.push(commit)
-    //       for (const parentHash of commit!.parent) {
-    //         const parentCommit = git.commits[parentHash]
-    //         stack.push({...parentCommit, hash:parentHash})
-    //       }
-    //     }
-    //   })
-    //   return result
-    // })
+    // 테스팅 용 객체 생성 부분
+    // const presentDir = new Directory('test1')
+    // const git = new Git(presentDir)
+    // const makeSwitchCommit = (prev: string ,next: string) => {
+    //   git.head = next
+    //   git.branches[next] = git.branches[prev]
+    //   const switchATxt = new PlainFile('switch_a.txt', presentDir)
+    //   git.add()
+    //   git.commit('Add switch text file A')
+    //   const switchBTxt = new PlainFile('switch_b.txt', presentDir)
+    //   git.add()
+    //   git.commit('Add switch text file B')
+    // }
+    // const makeSampleCommit = () => {
+    //   const aTxt = new PlainFile('a.txt', presentDir)
+    //   git.add()
+    //   git.commit('Add text file A')
+    //   const bTxt = new PlainFile('b.txt', presentDir)
+    //   git.add()
+    //   git.commit('Add text file B')
+    //   const cTxt = new PlainFile('c.txt', presentDir)
+    //   git.add()
+    //   git.commit('Add text file C')
+    //   const dTxt = new PlainFile('d.txt', presentDir)
+    //   git.add()
+    //   git.commit('Add text file D')
+    //   return git
+    // }
+    // makeSampleCommit()
+    // makeSwitchCommit('master', 'develop')
 
     const commits = computed(() => {
-      // const { problem } = props
-      // const { git } = problem
-      
       // 아직 git init을 하지 않은 경우
-      if (git == null) {
+      if (props.problem.git == null) {
         return []
       }
 
@@ -135,10 +73,10 @@ export default defineComponent({
       const result = []
         
       // head : 브랜치 이름
-      const head = git.head
+      const head = props.problem.git.head
 
       // headCommitHash : 해당 브랜치의 가장 최근 커밋 해쉬
-      const headCommitHash = git.branches[head]
+      const headCommitHash = props.problem.git.branches[head]
 
       // 커밋 기록이 없는 경우
       if (headCommitHash === '') {
@@ -146,7 +84,7 @@ export default defineComponent({
       }
 
       // 기준이 될 commit 노드 설정
-      const headCommit = git.commits[headCommitHash]
+      const headCommit = props.problem.git.commits[headCommitHash]
 
       // 검사를 진행할 스택
       const stack = [
@@ -162,18 +100,15 @@ export default defineComponent({
         const commit = stack.pop()
         result.push(commit)
         for (const parentHash of commit!.parent) {
-          const parentCommit = git.commits[parentHash]
+          const parentCommit = props.problem.git.commits[parentHash]
           stack.push({...parentCommit, hash:parentHash})
         }
       }
       return result
     })
 
-
-
     return {
-      commits,
-      git
+      commits
     }
   },
 })
