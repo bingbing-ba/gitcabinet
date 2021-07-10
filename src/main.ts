5340f395672a16052b271a2ee6dc62133b6f8df3
bing <qlstks@gmail.com>
2021-07-10 16:21:45 +0900

    firebase 앱 설정(analytics 위한)

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,20 @@
+import firebase from 'firebase/app'
+import 'firebase/analytics'
+
 import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDwSeYTxxqY4XDcC6b2q6Geo88hRh4619Q",
+  authDomain: "gitcabinet-bing.firebaseapp.com",
+  projectId: "gitcabinet-bing",
+  storageBucket: "gitcabinet-bing.appspot.com",
+  messagingSenderId: "73400342457",
+  appId: "1:73400342457:web:2590bfc48f7873e0624868",
+  measurementId: "G-DD1GZGGSYX"
+};
+firebase.initializeApp(firebaseConfig)
+firebase.analytics()
 
 createApp(App).mount('#app')
